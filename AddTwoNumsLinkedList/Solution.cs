151b4efd5eb337e48d3c6717ccedf16e80fe29b7
RakodilGena <rakodilgenathefirst@gmail.com>
2023-12-14 17:55:15 +0300

    initial

---
+++ AddTwoNumsLinkedList/Solution.cs
@@ -0,0 +1,89 @@
+﻿namespace AddTwoNumsLinkedList;
+
+public sealed class ListNode
+{
+    public int val;
+    public ListNode? next;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+internal sealed class Solution
+{
+    public ListNode AddTwoNumbers(
+        ListNode l1,
+        ListNode l2)
+    {
+        var current1 = l1;
+        var finished1 = false;
+        
+        var current2 = l2;
+        var finished2 = false;
+
+        int incNext = 0;
+
+        ListNode? resultNode = null;
+        ListNode? prevResultNode = null;
+            
+        while (true)
+        {
+            int first = !finished1? current1.val : 0;
+            int second = !finished2? current2.val : 0;
+
+            int sum = first + second + incNext;
+
+            int currentResult = sum % 10;
+            incNext = sum <= 9 ? 0 : 1;
+
+            var currentNode = new ListNode(currentResult);
+            if (prevResultNode is null)
+            {
+                resultNode = prevResultNode = currentNode;
+            }
+            else
+            {
+                prevResultNode.next = currentNode;
+                prevResultNode = currentNode;
+            }
+
+            if (!finished1)
+            {
+                if (current1.next is not null)
+                {
+                    current1 = current1.next;
+                }
+                else
+                {
+                    finished1 = true;
+                }
+            }
+            
+
+            if (!finished2)
+            {
+                if (current2.next is not null)
+                {
+                    current2 = current2.next;
+                }
+                else
+                {
+                    finished2 = true;
+                }
+            }
+
+
+            if (finished1 && finished2)
+            {
+                if (incNext is 1)
+                    currentNode.next = new ListNode(1); 
+                break;
+            }
+        }
+
+        return resultNode!;
+    }
+}
---
+++ TwoSum/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace TwoSum;
+
+public sealed class Solution
+{
+    public int[] TwoSumNaive(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return [i, j];
+            }
+        }
+
+        return [];
+    }
+
+
+    public int[] TwoSumSpans(int[] nums, int target)
+    {
+        var arraySpan = nums.AsSpan();
+        
+        for (int i = 0; i < arraySpan.Length - 1; i++)
+        {
+            int number = arraySpan[i];
+
+            int searchTarget = target - number;
+
+            var span = arraySpan[(i + 1)..];
+
+            var indexOfSearchTarget = span.IndexOf(searchTarget);
+
+            if (indexOfSearchTarget is not -1)
+                return [i, indexOfSearchTarget + i + 1];
+        }
+
+        return [];
+    }
+    
+    public int[] TwoSumDictionary(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>(nums.Length-1);
+        
+        var arraySpan = nums.AsSpan();
+        
+        for (int i = 0; i < arraySpan.Length; i++)
+        {
+            int number = arraySpan[i];
+
+            int searchTarget = target - number;
+            
+            var indexOfSearchTarget = dict.GetValueOrDefault(searchTarget, -1);
+            if (indexOfSearchTarget is not -1)
+                return [indexOfSearchTarget, i];
+            
+            dict.TryAdd(number, i);
+        }
+
+        return [];
+    }
+}
